39c4b926c6e06317187467a58c77fe204b5712fa
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2018-12-29 16:33:37 +0100

    changes

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -91,20 +91,7 @@ export default class AdminController {
   }
 
     static signIn(req, res){
-
-      if (req.method === 'OPTIONS') {
-        console.log('!OPTIONS');
-        var headers = {};
-        // IE8 does not allow domains to be specified, just the *
-        // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-        headers["Access-Control-Allow-Origin"] = "*";
-        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-        headers["Access-Control-Allow-Credentials"] = false;
-        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-        res.writeHead(200, headers);
-        res.end();
-  }else{
+      
        
       let {username,password } = req.body;
       var query = 'select * from administrator WHERE username="'+username+'"';
@@ -141,7 +128,7 @@ export default class AdminController {
     
    
     
-  }
+   
     
      
     }
