5b235e64f0fafc4148f3491322f2ea3f4dd776e8
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2018-12-29 15:18:56 +0100

    database

--- database/mysqlLib.js
+++ database/mysqlLib.js
@@ -1,11 +1,18 @@
 import mysql from 'mysql'; 
 
   
-var pool = mysql.createPool({
+/*var pool = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
     password : 'password',
     database : 'lotus'
+});*/
+
+var pool = mysql.createPool({
+  host     : 'us-cdbr-iron-east-01.cleardb.net',
+  user     : 'b37e36e1d6a9a4',
+  password : '4a351487',
+  database : 'heroku_d17fb34a6b1ec3e'
 });
 
 
