266e2d6c9c54a12a09f41986e3f4011a6d0f2b89
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2018-12-29 20:19:40 +0100

    changes

--- database/mysqlLib.js
+++ database/mysqlLib.js
@@ -1,19 +1,19 @@
 import mysql from 'mysql'; 
 
   
-var pool = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'lotus'
-});
-
 // var pool = mysql.createPool({
-//   host     : 'us-cdbr-iron-east-01.cleardb.net',
-//   user     : 'b37e36e1d6a9a4',
-//   password : '4a351487',
-//   database : 'heroku_d17fb34a6b1ec3e'
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'password',
+//     database : 'lotus'
 // });
+
+var pool = mysql.createPool({
+  host     : 'us-cdbr-iron-east-01.cleardb.net',
+  user     : 'b37e36e1d6a9a4',
+  password : '4a351487',
+  database : 'heroku_d17fb34a6b1ec3e'
+});
 
 
 
