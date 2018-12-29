471e03d6046c2f1cd720380c0680fb611f073a92
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2018-12-29 16:35:29 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,7 @@ import MemberController from '../controllers/Member';
 
 const router = new Router();
 
-if (req.method === 'OPTIONS') {
-    console.log('!OPTIONS');
-    var headers = {};
-    // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-    headers["Access-Control-Allow-Credentials"] = false;
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-    res.writeHead(200, headers);
-    res.end();
-}
+
 
 router.get('/admin',authMiddleware, AdminController.index);
 router.post('/admin/',authMiddleware, AdminController.create);
