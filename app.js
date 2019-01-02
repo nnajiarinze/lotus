e26911d578b4830ab590a6319ad3d34ec0101356
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2019-01-02 17:24:41 +0100

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import parser from 'body-parser';
-
+import cors from 'cors';
 import router from './routes';
 //import authMiddleware from './controllers/Auth';
 var port = process.env.PORT || 3000;
