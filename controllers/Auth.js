abc5347b880f7a2c818d1d1161cb02291e31e5af
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2019-01-02 16:45:56 +0100

    changes

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -19,9 +19,9 @@ module.exports = function (req, res, next) {
         return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
       }else{
       
-        res.setHeader('Access-Control-Allow-Origin', '*');
+       // res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Expose-Headers','Content-Length');
-        res.setHeader("Access-Control-Allow-Credentials", true);
+        //res.setHeader("Access-Control-Allow-Credentials", false);
         res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, DELETE, OPTIONS');
         res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         return next();
