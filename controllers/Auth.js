3e6714521b65bc9ef62df917ded68ac76e8ceb75
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2019-01-02 15:50:04 +0100

    changes

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -4,19 +4,21 @@ import config from '../config/config';
 
 
 module.exports = function (req, res, next) {
- console.log(req);
-    var token = req.headers['x-access-token'];
-        
+ //console.log(req);
+    var token = req.headers['authorization'];
+  console.log(req.headers);
+        console.log(token);
     if(!token){
       return res.status(401).send({auth: false, message: 'No token provided' });
     }
-
+    token = token.replace('Bearer', '');
+    token = token.trim();
+    console.log(' token is ' +token);
     jwt.verify(token, config.secret, function(err, decoded) {
       if (err) {
         return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
       }else{
-
-
+      
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Expose-Headers','Content-Length');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
