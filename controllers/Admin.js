be4b7f378fa745684ba6a0dc0d3342a4ba9488e2
Arinze Nnaji <Arinze.Nnaji@fcmb.com>
2019-01-04 12:59:55 +0100

    changes

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -53,27 +53,7 @@ export default class AdminController {
     }
   }
 
-  // static getUserNames(url) {
-  //   return new Promise(function (resolve, reject) {
-  //     const xhr = new XMLHttpRequest();
-  //     xhr.timeout = 2000;
-  //     xhr.onreadystatechange = function(e) {
-  //       if (xhr.readyState === 4) {
-  //         if (xhr.status === 200) {
-  //           resolve(xhr.response)
-  //         } else {
-  //           reject(xhr.status)
-  //         }
-  //       }
-  //     }
-  //     xhr.ontimeout = function () {
-  //       reject('timeout')
-  //     }
-  //     xhr.open('get', url, true)
-  //     xhr.send();
-  //   })
-  // }
-
+  
 
   static getUserName(token) {
   
